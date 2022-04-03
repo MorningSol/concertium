7ee4019f293d29e0c134fd6062949f8f528f464c
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-04-03 19:16:43 -0400

    create event cards done, issue with given css.  fix to follow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var ytPlayerEl = document.querySelector("#ytplayer");
 var citySearchBtnEl = document.querySelector("#city-search")
 var cityInputEl = document.querySelector("#city-input")
-
+var resultsBox = document.querySelector("#results-box")
 
 var getVideoData = function(band){
     var videoApi = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + band + "&maxResults=1&type=video&key=AIzaSyCbq62d8uqQFXIYt2QwFKC3x2we8t8KYEc"
@@ -55,17 +55,64 @@ var createEventCard = function(eventTitle, eventDate, eventVenue, eventUrl ,even
         var performer = eventPerformers[i].name;
         var performerImage = eventPerformers[i].image;
         
-        // var eventCard = document.createElement("div");
-        // eventCard.classList = "Card";
+        var div1 = document.createElement("div");
+        div1.classList = "column is-4-tablet is-3-desktop";
+        var eventCard = document.createElement("div");
+        eventCard.classList = "card";
+        var cardHeader = document.createElement("div");
+        cardHeader.classList = "media has-text-centered px-6";
+        var div2 = document.createElement("div");
+        div2.classList = "media-content mt-4";
+        var date = document.createElement("p");
+        date.textContent = eventDate
+        var title = document.createElement("p");
+        title.classList = "title is-4 mt-1";
+        title.textContent = performer;
+        var venue = document.createElement("p");
+        venue.classList = "subtitle is-5";
+        venue.textContent = eventVenue;
 
-        // var cardTitle = document.createElement("");
-        // cardTitle.classList= "card-header";
+        div2.appendChild(date);
+        div2.appendChild(title);
+        div2.appendChild(venue);
+        cardHeader.appendChild(div2);
 
-        // var cardImage = document.createElement("");
-        // cardImage.classList = "card-image";
+        var cardImage = document.createElement("div");
+        cardImage.classList = "card-image";
+        var figure = document.createElement("figure");
+        figure.classList = "image is-4by3";
+        var bandImg = document.createElement("img");
+        bandImg.src = performerImage;
 
-        // var cardInfo = document.createElement("");
-        // cardInfo.classList = "card-content";
+        figure.appendChild(bandImg);
+        cardImage.appendChild(figure);
+        
+        var cardContent = document.createElement("div");
+        cardContent.classList = "card-content block";
+        var div3 = document.createElement("div");
+        div3.classList = "content";
+        var ticketBtn = document.createElement("div");
+        ticketBtn.classList = "control";
+        var button1 = document.createElement("button");
+        button1.classList = "button is-large is-fullwidth is-warning" 
+        button1.textContent = "Buy a Ticket"
+        var modalBtn = document.createElement("div");
+        modalBtn.classList = "control mt-2 block";
+        var button2 = document.createElement("button");
+        button2.classList = "button is-large is-fullwidth is-warning";
+        button2.textContent = "Listen Here";
+
+        ticketBtn.appendChild(button1)
+        modalBtn.appendChild(button2)
+        div3.appendChild(ticketBtn)
+        div3.appendChild(modalBtn)
+        cardContent.appendChild(div3);
+
+        eventCard.appendChild(cardHeader)
+        eventCard.appendChild(cardImage)
+        eventCard.appendChild(cardContent)
+        div1.appendChild(eventCard);
+        resultsBox.appendChild(div1);
 
     }
 
@@ -83,5 +130,3 @@ var searchButtonHandler = function(){
 citySearchBtnEl.addEventListener("click",searchButtonHandler)
 
 
-// getEventData("ottawa");
-// getVideoData("billy talent");
